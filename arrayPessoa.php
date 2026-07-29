9d8e0d7d810fac1fc17e73446ead37bda3308d9f
Kauã <engs-kauadelgado@camporeal.edu.br>
2026-07-29 20:43:51 -0300

    array de pessoa

---
+++ arrayPessoa.php
@@ -0,0 +1,13 @@
+<?php
+
+$pessoa = array(
+    "Nome" => "Carlos",
+    "Idade" => 20,
+    "Cidade" => "Curitiba",
+    "Tchola" => "Homi"
+);
+
+echo "Nome: " . $pessoa["Nome"] . "\n";
+echo "Idade: " . $pessoa["Idade"] . "\n";
+echo "Cidade: " . $pessoa["Cidade"] . "\n";
+echo "Tchola: " . $pessoa["Tchola"] . "\n";
