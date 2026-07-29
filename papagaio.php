3c279f6a24f4df04a56cc9cbd01acfe8180a559a
Kauã <engs-kauadelgado@camporeal.edu.br>
2026-07-29 20:40:35 -0300

    raças de papagaio

---
+++ papagaio.php
@@ -0,0 +1,13 @@
+<?php
+
+$papagaios = array(
+    "Papagaio-verdadeiro",
+    "Papagaio-do-mangue",
+    "Papagaio-de-cara-roxa"
+);
+
+$papagaios[] = "Papagaio-da-serra";
+
+foreach ($papagaios as $papagaio) {
+    echo $papagaio . "\n";
+}
