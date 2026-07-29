37fde5061e88b3089bcf0f139cd3537e59ba86d5
Kauã <engs-kauadelgado@camporeal.edu.br>
2026-07-29 20:37:06 -0300

    array com cinco memes

---
+++ memes.php
@@ -0,0 +1,13 @@
+<?php
+
+$memes = array(
+    "Calma calabreso",
+    "Troll Face",
+    "Fake Natty",
+    "DOZEE",
+    "Alexa ligar o ar"
+);
+
+foreach ($memes as $meme) {
+    echo $meme . "\n";
+}
