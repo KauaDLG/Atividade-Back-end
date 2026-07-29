3809da2f77a69e35b269a6f5882860de087e75f5
Kauã <engs-kauadelgado@camporeal.edu.br>
2026-07-29 20:29:26 -0300

    função que recebe dois números e retorna a soma, a subtração e uma frase incrível

---
+++ soma_subtracao_frase.php
@@ -0,0 +1,13 @@
+<?php
+
+function calcular($num1, $num2) {
+
+    $soma = $num1 + $num2;
+    $subtracao = $num1 - $num2;
+
+    echo "Soma: $soma \n";
+    echo "Subtração: $subtracao \n";
+    echo "Frase incrível: Hello World";
+}
+
+calcular(20, 8);
